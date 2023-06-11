2d85bd7b94edd93c2611919e8a36d91ed1f28398
sverdiyev <oleksandr.verdiyev@devbridge.com>
2023-06-11 23:35:47 +0200

    cleanup controller

--- src/main/java/com/example/controller/MyController.java
+++ src/main/java/com/example/controller/MyController.java
@@ -2,14 +2,13 @@ package com.example.controller;
 
 import java.util.HashMap;
 import java.util.Map;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
 
-  @GetMapping(value = "/elo", produces = MediaType.APPLICATION_JSON_VALUE)
+  @GetMapping(value = "/elo")
   public Map<String, String> hello() {
     HashMap<String, String> response = new HashMap<>();
     response.put("message", "Hello from controller");
