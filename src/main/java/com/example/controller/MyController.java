0e7f6f15c9c455271ae2dbcefcf5d56973044180
sverdiyev <oleksandr.verdiyev@devbridge.com>
2023-06-11 22:33:26 +0200

    add multiple endpoints

--- src/main/java/com/example/controller/MyController.java
+++ src/main/java/com/example/controller/MyController.java
@@ -3,17 +3,31 @@ package com.example.controller;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MyController {
 
-  @RequestMapping("/")
+  @GetMapping("/elo")
   @ResponseBody
-  public Map<String, String> helloWorld() {
+  public Map<String, String> hello() {
     HashMap<String, String> response = new HashMap<>();
-    response.put("message", "Hello World");
+    response.put("message", "Hello from controller");
+    return response;
+  }
+
+  @GetMapping("/foo")
+  @ResponseBody
+  public String foo() {
+    return "foo";
+  }
+
+  @GetMapping("/bar")
+  @ResponseBody
+  public Map<String, String> bar() {
+    HashMap<String, String> response = new HashMap<>();
+    response.put("message", "bar");
     return response;
   }
 }
