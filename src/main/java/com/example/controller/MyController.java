ab070996bd80adf2305a674debfab9a5e1807b4b
sverdiyev <oleksandr.verdiyev@devbridge.com>
2023-06-11 22:59:55 +0200

    reformat controller

--- src/main/java/com/example/controller/MyController.java
+++ src/main/java/com/example/controller/MyController.java
@@ -3,15 +3,13 @@ package com.example.controller;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MyController {
 
   @GetMapping(value = "/elo", produces = MediaType.APPLICATION_JSON_VALUE)
-  @ResponseBody
   public Map<String, String> hello() {
     HashMap<String, String> response = new HashMap<>();
     response.put("message", "Hello from controller");
@@ -19,7 +17,6 @@ public class MyController {
   }
 
   @GetMapping("/foo")
-  @ResponseBody
   public String foo() {
     return "foo";
   }
