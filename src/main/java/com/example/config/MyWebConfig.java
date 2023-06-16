b11b5a50d03e73268684b415347c541ceaf2479c
sverdiyev <oleksandr.verdiyev@devbridge.com>
2023-06-16 11:44:49 +0200

    add cors support

--- src/main/java/com/example/config/MyWebConfig.java
+++ src/main/java/com/example/config/MyWebConfig.java
@@ -2,14 +2,22 @@ package com.example.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.example")
-public class MyWebConfig {
+public class MyWebConfig implements WebMvcConfigurer {
 
   public MyWebConfig() {
     System.out.println("HELLO FROM CONFIG");
+  }
+
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**");
   }
 }
