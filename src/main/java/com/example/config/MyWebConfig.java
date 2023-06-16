0b783266872a7a1faf2056dc0e06ae6554eec82f
sverdiyev <oleksandr.verdiyev@devbridge.com>
2023-06-16 12:43:27 +0200

    !!! add websocket handlers and configuration and CORS configuration

--- src/main/java/com/example/config/MyWebConfig.java
+++ src/main/java/com/example/config/MyWebConfig.java
@@ -1,15 +1,20 @@
 package com.example.config;
 
+import com.example.controller.MyWebsocketHandler;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.example")
-public class MyWebConfig implements WebMvcConfigurer {
+@EnableWebSocket
+public class MyWebConfig implements WebMvcConfigurer, WebSocketConfigurer {
 
   public MyWebConfig() {
     System.out.println("HELLO FROM CONFIG");
@@ -18,6 +23,12 @@ public class MyWebConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**");
+    registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
   }
+
+  @Override
+  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    registry.addHandler(new MyWebsocketHandler(), "/chat").setAllowedOrigins("*");
+  }
+
 }
---
+++ src/main/java/com/example/controller/MyWebsocketHandler.java
@@ -0,0 +1,18 @@
+package com.example.controller;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class MyWebsocketHandler extends TextWebSocketHandler {
+
+  public MyWebsocketHandler() {
+    System.out.println("hello from websocket handler");
+  }
+
+  @Override
+  protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+    System.out.println("message received");
+  }
+
+}
