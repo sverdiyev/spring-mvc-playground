acb628d49ad0484e098183d11a2bd7a2cfba4bb5
sverdiyev <oleksandr.verdiyev@devbridge.com>
2023-06-21 21:29:20 +0200

    enable yml files for application properties

--- src/main/java/com/example/config/ApplicationPropertiesFileConfig.java
+++ src/main/java/com/example/config/ApplicationPropertiesFileConfig.java
@@ -1,20 +1,29 @@
 package com.example.config;
 
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
-@PropertySource("classpath:application.properties")
+@PropertySource("classpath:application.yml")
 public class ApplicationPropertiesFileConfig {
 
 
   @Bean
   //needs to be static to allow spring context to resolve the application files early enough
   public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
-    return new PropertySourcesPlaceholderConfigurer();
+
+    var properties = new PropertySourcesPlaceholderConfigurer();
+
+    YamlPropertiesFactoryBean yaml = new YamlPropertiesFactoryBean();
+    yaml.setResources(new ClassPathResource("application.yml"));
+
+    properties.setProperties(yaml.getObject());
+
+    return properties;
   }
 
-  //need to configure YamlPropertiesFactoryBean to work with yaml files
 }
